dafcbec6f42ddf0e49bdd498f5b8de4e428c53ec
Hector Barazorda <ph.barazorda@gmail.com>
2024-02-22 10:02:58 -0500

    feat: :art: config to sky

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,35 @@
 import { Nunito } from 'next/font/google'
 
-import Navbar from '@/app/components/navbar/Navbar';
-import LoginModal from '@/app/components/modals/LoginModal';
-import RegisterModal from '@/app/components/modals/RegisterModal';
-import SearchModal from '@/app/components/modals/SearchModal';
-import RentModal from '@/app/components/modals/RentModal';
+import Navbar from '@/app/components/navbar/Navbar'
+import LoginModal from '@/app/components/modals/LoginModal'
+import RegisterModal from '@/app/components/modals/RegisterModal'
+import SearchModal from '@/app/components/modals/SearchModal'
+import RentModal from '@/app/components/modals/RentModal'
 
-import ToasterProvider from '@/app/providers/ToasterProvider';
+import ToasterProvider from '@/app/providers/ToasterProvider'
 
 import './globals.css'
-import ClientOnly from './components/ClientOnly';
-import getCurrentUser from './actions/getCurrentUser';
+import ClientOnly from './components/ClientOnly'
+import getCurrentUser from './actions/getCurrentUser'
 
 export const metadata = {
   title: 'Airbnb',
-  description: 'Airbnb Clone',
+  description: 'Airbnb Clone'
 }
 
-const font = Nunito({ 
-  subsets: ['latin'], 
-});
+const font = Nunito({
+  subsets: ['latin']
+})
 
-export default async function RootLayout({
-  children,
+export default async function RootLayout ({
+  children
 }: {
   children: React.ReactNode
 }) {
-  const currentUser = await getCurrentUser();
+  const currentUser = await getCurrentUser()
 
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
@@ -39,9 +39,7 @@ export default async function RootLayout({
           <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        <div className="pb-20 pt-28">
-          {children}
-        </div>
+        <div className='pb-20 pt-40'>{children}</div>
       </body>
     </html>
   )
