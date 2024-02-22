d6a21dbba6764f35b4ae78c5a59f58391855cc4e
Hector Barazorda <ph.barazorda@gmail.com>
2024-02-22 10:05:24 -0500

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ import ClientOnly from './components/ClientOnly'
 import getCurrentUser from './actions/getCurrentUser'
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone'
+  title: 'Sky house',
+  description: 'website to sell properties'
 }
 
 const font = Nunito({
